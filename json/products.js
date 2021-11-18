c791c62d458900be866acfc8ab083c4cae67d5f2
Andrey Kiryanov <39236657+AndreyK1ryanov@users.noreply.github.com>
2021-11-18 22:54:53 +0500

    daily_update

--- json/products.js
+++ json/products.js
@@ -1,32 +1,34 @@
-var product = {
+{
     "active": true,
     "category": [
         {
-            "jackets": [{
-                "name": "Дутик",
-                "img": "",
-                "price": "4999",
-                "oldPrice": "6000",
-            },
-            {
-                "name": "Вздутик",
-                "img": "",
-                "price": "2999",
-                "oldPrice": "4000",
-            }
+            "jackets": [
+                {
+                    "name": "Дутик",
+                    "img": "",
+                    "price": "4999",
+                    "oldPrice": "6000"
+                },
+                {
+                    "name": "Вздутик",
+                    "img": "",
+                    "price": "2999",
+                    "oldPrice": "4000"
+                }
             ],
-            "outerwear": [{
-                "name": "Фуфайка",
-                "img": "",
-                "price": "8000",
-                "oldPrice": "9000",
-            },
-            {
-                "name": "Телогрейка",
-                "img": "",
-                "price": "3000",
-                "oldPrice": "7000",
-            }
+            "outerwear": [
+                {
+                    "name": "Фуфайка",
+                    "img": "",
+                    "price": "8000",
+                    "oldPrice": "9000"
+                },
+                {
+                    "name": "Телогрейка",
+                    "img": "",
+                    "price": "3000",
+                    "oldPrice": "7000"
+                }
             ]
         }
     ]
