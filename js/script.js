be6f19783f6e2dd9e5c25ce81283d463c9ca3ad3
Andrey Kiryanov <39236657+AndreyK1ryanov@users.noreply.github.com>
2021-11-19 12:39:40 +0500

    daily_update

--- js/script.js
+++ js/script.js
@@ -8,4 +8,34 @@ $(document).ready(function () {
         autoplaySpeed: 1700,
         arrows: false,
     });
+
+
+    $.getJSON("products.json", function (json) {
+        console.log(json); // this will show the info it in firebug console
+    });
+
+
+
+
+
+    /*var requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
+    var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function () {
+        var product = request.response;
+        populateHeader(product);
+        showHeroes(product);
+    }
+    function populateHeader(jsonObj) {
+    var myH1 = document.createElement('h1');
+    myH1.textContent = jsonObj['squadName'];
+    header.appendChild(myH1);
+
+    var myPara = document.createElement('p');
+    myPara.textContent = 'Hometown: ' + jsonObj['homeTown'] + ' // Formed: ' + jsonObj['formed'];
+    header.appendChild(myPara);
+}
+    */
 });
