de8832fb85ff6ae35e9a192117cdbbc4220ef687
Andrey Kiryanov <39236657+AndreyK1ryanov@users.noreply.github.com>
2021-11-25 22:37:09 +0500

    daily_update

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
 $(document).ready(function () {
+
     $('.promo-slider').slick({
         dots: false,
-        infinite: true,
-        speed: 700,
-        autoplay: false,
         fade: true,
-        autoplaySpeed: 2000,
         arrows: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        adaptiveWidth: true
     });
 
     $('.about-us__slider').slick({
         dots: false,
-        infinite: true,
+        infinite: false,
         speed: 700,
         autoplay: false,
         fade: true,
@@ -29,41 +31,7 @@ $(document).ready(function () {
             },
         ]
     });
-    $('.responsive').slick({
-        dots: true,
-        infinite: false,
-        speed: 300,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-        ]
-    });
+
 
     $('.product-box').hover(function () {
         $('.product-box__price', this).hide();
