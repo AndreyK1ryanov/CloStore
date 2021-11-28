329c7db03d8d05cfc59b135594700606b479223e
Andrey Kiryanov <39236657+AndreyK1ryanov@users.noreply.github.com>
2021-11-28 17:58:57 +0500

    daily_update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 $(document).ready(function () {
+    $('.header-menu__open-button').on('click', function () {
+        $('.header-menu__open-button').removeClass('header-menu__open-button_activate').addClass('header-menu__open-button_deactivate');
+        $('.header-menu__navigation').removeClass('header-menu__navigation_deactivate').addClass('header-menu__navigation_activate');
+        $('.header-menu__close-button').removeClass('header-menu__close-button_deactivate').addClass('header-menu__close-button_activate');
+        $('.mobile-filter').removeClass('mobile-filter_activate').addClass('mobile-filter_deactivate')
+    });
+    $('.header-menu__close-button').on('click', function () {
+        $('.header-menu__close-button').removeClass('header-menu__close-button_activate').addClass('header-menu__close-button_deactivate');
+        $('.header-menu__navigation').removeClass('header-menu__navigation_activate').addClass('header-menu__navigation_deactivate');
+        $('.header-menu__open-button').removeClass('header-menu__open-button_deactivate').addClass('header-menu__open-button_activate');
+        $('.mobile-filter').removeClass('mobile-filter_deactivate').addClass('mobile-filter_activate')
+    });
 
     $('.promo-slider').slick({
         dots: false,
@@ -19,6 +31,9 @@ $(document).ready(function () {
         $('.product-box__price', this).show();
     });
 
+    $('.mobile-filter__button').on('click', function () {
+        alert('Благодарю за нажатие, но данная страница не подключена к CMS')
+    })
     $('.subscribe-form-body__button').on('click', function () {
         alert('Благодарю за нажатие, но данная страница не подключена к CMS')
     })
@@ -30,5 +45,18 @@ $(document).ready(function () {
         $('.select-sex__button').removeClass('select-sex__button_active').addClass('button-white').addClass('select-sex__button_deactive')
         $(this).addClass('select-sex__button_active').removeClass('button-white').addClass('button-black').removeClass('select-sex__button_deactive')
     })
+
+    function myFunction() {
+        if ($(window).width() <= 480) {
+            $('.gallery-box__send-mail-button').removeClass('button-size-m').addClass('button-size-s')
+        }
+        else {
+            $('.gallery-box__send-mail-button').removeClass('button-size-s').addClass('button-size-m')
+        }
+    }
+    myFunction();
+    $(window).resize(function () {
+        myFunction();
+    });
 });
 
