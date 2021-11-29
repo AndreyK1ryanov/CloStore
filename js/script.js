8255043846d1fcc475bc594932971c99264c10de
Andrey Kiryanov <39236657+AndreyK1ryanov@users.noreply.github.com>
2021-11-29 14:02:29 +0500

    finish update Alpha 0.1v

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
+//Author: Andrey Kiryanov
+//Date: 29.11.2021
+//Version: Alpha 0.1v
 $(document).ready(function () {
+
+    /*********************** Mobile menu header button's script - start ****************/
     $('.header-menu__open-button').on('click', function () {
         $('.header-menu__open-button').removeClass('header-menu__open-button_activate').addClass('header-menu__open-button_deactivate');
         $('.header-menu__navigation').removeClass('header-menu__navigation_deactivate').addClass('header-menu__navigation_activate');
@@ -11,7 +16,9 @@ $(document).ready(function () {
         $('.header-menu__open-button').removeClass('header-menu__open-button_deactivate').addClass('header-menu__open-button_activate');
         $('.mobile-filter').removeClass('mobile-filter_deactivate').addClass('mobile-filter_activate')
     });
+    /*********************** Mobile menu header button's script - finish ****************/
 
+    /**************************** slick-slider script - start **************************/
     $('.promo-slider').slick({
         dots: false,
         fade: true,
@@ -24,13 +31,17 @@ $(document).ready(function () {
         adaptiveHeight: true,
         adaptiveWidth: true
     });
+    /*************************** slick-slider script - finish **************************/
 
+    /**** when hover on product box this script hidden price block - start function ****/
     $('.product-box').hover(function () {
         $('.product-box__price', this).hide();
     }, function () {
         $('.product-box__price', this).show();
     });
+    /***** when hover on product box this script hidden price block - finish function ***/
 
+    /********************************** button - gap - start ***************************/
     $('.mobile-filter__button').on('click', function () {
         alert('Благодарю за нажатие, но данная страница не подключена к CMS')
     })
@@ -45,7 +56,9 @@ $(document).ready(function () {
         $('.select-sex__button').removeClass('select-sex__button_active').addClass('button-white').addClass('select-sex__button_deactive')
         $(this).addClass('select-sex__button_active').removeClass('button-white').addClass('button-black').removeClass('select-sex__button_deactive')
     })
+    /********************************** button - gap - finish ***************************/
 
+    /***** change button in about us block when window size >480px - start function *****/
     function myFunction() {
         if ($(window).width() <= 480) {
             $('.gallery-box__send-mail-button').removeClass('button-size-m').addClass('button-size-s')
@@ -58,5 +71,7 @@ $(document).ready(function () {
     $(window).resize(function () {
         myFunction();
     });
+    /*** change button in about us block when window size >480px - finish function *****/
+
 });
 
